5a63f794dac72e61c88c3d64ebe9961070a2bf3b
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2021-03-17 21:29:39 -0400

    workflows

--- bible.py
+++ bible.py
@@ -6,25 +6,37 @@ import requests
 import json
 import sys
 
-border = "***"
+BORDER = "***"
 
-q = input("> ") if len(sys.argv) < 2 else ' '.join(sys.argv[1:])
-# get data
-base = "https://getbible.net/json?"
-url = base + "passage=" + q + "&" + "v=kjv"
-r = requests.get(url)
-t = r.text[1:-2]
-j = json.loads(t)
 
-print(border)
+def search(q):
+    # get data
+    base = "https://getbible.net/json?"
+    url = base + "passage=" + q + "&" + "v=kjv"
+    r = requests.get(url)
+    t = r.text[1:-2]
+    j = json.loads(t)
 
-# main loop
-print("Book: " + j["book"][0]["book_name"])
-for ch in j["book"]:
-    print("Chapter: " + str(ch["chapter_nr"]))
-    for vn in ch["chapter"]:
-        v = ch["chapter"][vn]
-        print(str(v["verse_nr"]) + " " + str(v["verse"].strip()))
+    print(BORDER)
 
-print(border)
+    # main loop
+    print("Book: " + j["book"][0]["book_name"])
+    for ch in j["book"]:
+        print("Chapter: " + str(ch["chapter_nr"]))
+        for vn in ch["chapter"]:
+            v = ch["chapter"][vn]
+            print(str(v["verse_nr"]) + " " + str(v["verse"].strip()))
 
+    print(BORDER)
+
+
+if __name__ == "__main__":
+    try:
+        q = input("> ") if len(sys.argv) < 2 else ' '.join(sys.argv[1:])
+        search(q)
+    except json.decoder.JSONDecodeError:
+        print("ERROR: Invalid input.")
+        exit(-1)
+    except:
+        print("ERROR: An unknown error occurred.")
+        exit(-1)
